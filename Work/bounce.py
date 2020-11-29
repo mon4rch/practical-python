8c373d89ffe99808a0ec78aaacf07798ee6cc649
Laith <mon4rch@localhost.localdomain>
2020-11-29 08:55:36 -0500

    Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
-#
+print('Bouncing ball exercise\n')
+init_height =100.0
+print('Initially ball height is ',init_height)
+for bounce_count in range(1,11):
+    new_height = 0.6 * init_height
+    init_height = new_height
+    print(bounce_count, round(new_height, 4))#
+
 # Exercise 1.5
