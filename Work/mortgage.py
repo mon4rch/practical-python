1d831d226bad87ff40b4a0319aac008e01ed564f
Laith <mon4rch@localhost.localdomain>
2020-11-29 10:57:25 -0500

    Exercise 1.9: Extra pay calc

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
-# mortgage.py. Exercise 1.7 Dave's Mortgage
+# mortgage.py. Exercise 1.9 Dave's Mortgage
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 num_of_months = 0
-extra_pay = 1000.0
+extra_pay_start_month = int(input("Enter start month\n"))
+extra_pay_end_month = int(input("Enter end month\n"))
+extra_pay = float(input("Enter the extra payment\n"))
+month_range = extra_pay_end_month - extra_pay_start_month
 
-while num_of_months !=12:
+if month_range <= 0:
+	print("End month and start month values are incorrect!")
+	exit(1)
+
+while num_of_months !=month_range:
 	principal = principal * (1+rate/12) - (payment + extra_pay)
 	total_paid += (payment+extra_pay) 
 	num_of_months+=1
