86d38a33b96fba9938cd4ff3a121086135d814a8
Laith <mon4rch@localhost.localdomain>
2020-11-30 09:17:56 -0500

    Exercise 1.31 (1.30 + error handling)

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-# pcost.py Exercise 1.27: Reading a data file
-total_cost = 0
-shares=[]
-print("Reading file 'Portfolio.csv'...\n")
-with open('Data/portfolio.csv', 'rt') as file:
-    next(file)
-    for line in file:
-        line = line.strip('\n')
-        line = line.split(',')
-        total_cost += float(line[2])
+# pcost.py Exercise 1.30-31: create a function for reading the portfolio files + error handling
 
-    print(f'Total cost of shares is {round(total_cost, 4)}')
-    print('Closing file...\n')
+
+def portfolio_cost(filename):
+    'Calculates the total cost of shares in portfolio csv file'
+    total_cost = 0
+    print("Reading file 'Portfolio.csv'...\n")
+    with open(filename, 'rt') as file:
+        next(file)
+        for line in file:
+            line = line.strip('\n')
+            line = line.split(',')
+            try:
+                total_cost += float(line[2])
+            except (ValueError, TypeError, RuntimeError) as e:
+                print(e, 'Check your file for bad lines!\n')
+                pass
+        return total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost of shares is {round(cost, 4)}')
+
 
