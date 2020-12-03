5f3ff6ddedc556468b528149c46a633dd4b62e05
Laith <mon4rch@localhost.localdomain>
2020-12-03 12:39:53 -0500

    Exercise 3.3

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,43 @@
-# fileparse.py
-#
-# Exercise 3.3
+#!/usr/bin/python3 
+#fileparse.py Exercise 3.3 
+
+import csv
+
+def parse_csv(filename, select=None, types = [str, int, float], has_headers=False):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    with open(filename) as file:
+        rows = csv.reader(file)
+        #Read file headers
+        if has_headers:
+            headers = next(rows)
+        else:
+            if types and not has_headers:
+                holdings = {}
+                for row in rows:
+                    row = [func(val) for func, val in zip(types, row)]
+                    try:
+                        holdings[row[0]] = row[1]
+                    except (ValueError, IndexError):
+                        continue
+                print(holdings)
+                return [(v,k) for v, k in holdings.items()]
+
+        if select:
+            indices = [headers.index[colname] for colname in select]
+            headers = select
+        else:
+            indices = []
+        records = []
+        for row in rows:
+            if not row:       #skip blank lines
+                continue
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+            if indices:
+                row = [row[index] for index in indices]
+            record = dict(zip(headers, row))
+            records.append(record)
+            return records
+
--- Work/report.py
+++ Work/report.py
@@ -24,49 +24,45 @@ def read_portfolio(filename):
 def read_prices(filename):
 	'This functions reades prices from Data/prices.csv file'
 	print(f'Reading file {filename}...\n')
-	prices = []
+	prices = {}
 	with open(filename, 'rt') as file:
 		r = csv.reader(file)
-		headers = ['name', 'price']
 		for i, line in enumerate(r, start=1):
-		    record = dict(zip(headers, line))
-		    if record == {}:
+		    if line == {}:
 		    	print(f'no record at row {i}')
 		    	continue
 		    try:
-		    	
-		    	holding_dict = {
-		    	'name':  record['name'],
-		    	'price': float(record['price'])
-		    	}
-		    	prices.append(holding_dict)
-		    except ValueError:
-		    	print(f'\nRow{i}: Couldn\'t convert: {line}')
+		    	prices[line[0]]=float(line[1])
+		    except (ValueError, IndexError):
+		    	print(f'\nRow{i}: Couldn\'t convert: {line}\n\n')
 		    	pass
 		return prices
 
-def make_report(stocks, pricing):
-	print(pricing, type(pricing))
+def make_report(portfolio_data, stock_prices):
+	prices = read_prices(stock_prices)
+	portfolio = read_portfolio(portfolio_data)
 	dollar_sign = '$'
 	headers = ('Name', 'Shares', 'Price', 'Change')
 	#create x number of spaces between header values
-	s = ' ' * 6
+	s = ' ' * 4
 	print(s.join(headers))
-	print('-------- --------  --------  --------')
-	for i, line in enumerate(stocks):
-		price_prices = float(pricing[i]['price'])
+	print(('-' * 8 + ' ') * len(headers))
+	for line in portfolio:
+		price_prices = float(prices[line['name']])
 		price_portfolio = float(line['price'])
 		#value = abs(((price_prices - price_portfolio)/price_portfolio) * 100) Gain/Loss as a percentage
-		value = price_portfolio - price_prices  
-		print(f'{line["name"]:^10s}{line["shares"]:^10.2f} {dollar_sign}{line["price"]:<10.2f}{value:^10.2f}')
+		value = price_prices - price_portfolio  
+		print(f'{line["name"]:<10s}{line["shares"]:<10.2f} {dollar_sign}{line["price"]:<10.2f}{value:<15.2f}')
 		
-	return f'Program ran successfully'
+	return f'\nProgram ran successfully'
 	
 	
 	
 
 if __name__ == '__main__':
-	prices = read_prices('Data/prices.csv')
-	portfolio = read_portfolio('Data/portfoliodate.csv')
-	print(make_report(portfolio, prices))
+	if len(sys.argv) > 1:
+		make_report(sys.argv[1], sys.argv[2])
+	else:
+		print("You can run the program from the command line using this format:\npython3 report.py 'file1.csv' 'file2.csv'. Or, simply run python3 to run default files")
+		print(make_report('Data/portfolio.csv', 'Data/prices.csv'))
 
