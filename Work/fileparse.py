bf2ff79f971a133f5b4c63fd63aaa536730030b2
Laith <mon4rch@localhost.localdomain>
2020-12-04 12:37:14 -0500

    Exercise 3.10 + fixing errors

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,41 +3,41 @@
 
 import csv
 
-def parse_csv(filename, select=None, types = [str, int, float], has_headers=False):
+def parse_csv(filename, select=None, delimiter = ',', types = None, has_headers=True):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as file:
-        rows = csv.reader(file)
+        rows = csv.reader(file, delimiter = delimiter)
+        if select and not has_headers:
+            raise RuntimeError('Select is only possible with headers')
         #Read file headers
-        if has_headers:
+        if select and has_headers:
             headers = next(rows)
-        else:
-            if types and not has_headers:
-                holdings = {}
-                for row in rows:
-                    row = [func(val) for func, val in zip(types, row)]
-                    try:
-                        holdings[row[0]] = row[1]
-                    except (ValueError, IndexError):
-                        continue
-                print(holdings)
-                return [(v,k) for v, k in holdings.items()]
-
-        if select:
-            indices = [headers.index[colname] for colname in select]
+            print(headers)
+            indices = [headers.index(colname) for colname in select]
             headers = select
+            print(indices)
         else:
+            headers = []
             indices = []
+            
         records = []
-        for row in rows:
+        for i, row in enumerate(rows, start=1):
             if not row:       #skip blank lines
                 continue
             if types:
-                row = [func(val) for func, val in zip(types, row)]
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    print(f'Row{i}: Couldnt convert {row}\nReason:', e)
             if indices:
                 row = [row[index] for index in indices]
-            record = dict(zip(headers, row))
+
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
-            return records
+        return records
 
